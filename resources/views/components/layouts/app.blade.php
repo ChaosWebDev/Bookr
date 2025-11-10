5a1a4774434d578127581d76f99261b63577bcce
jpgerber1986 <jordangerber@gmail.com>
2025-11-10 16:14:27 -0700

    Add NerdFonts

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link href="https://www.nerdfonts.com/assets/css/webfont.css" rel="stylesheet" type="text/css" />
     @vite(['resources/styles/app.scss', 'resources/scripts/app.js'])
 </head>
 
