3bd4162bd350fde92e016e6286e8254f1a39dadf
jpgerber1986 <jordangerber@gmail.com>
2025-11-10 14:32:15 -0700

    Placeholder Route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 require_once __DIR__ . "/_console.php";
 require_once __DIR__ . "/_dev.php";
 
-Route::get('/',Dashboard::class)->name('dashboard');
+Route::get('/', fn() => "Dashboard")->name('dashboard');
