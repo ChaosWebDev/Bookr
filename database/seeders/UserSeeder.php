245327715665bf466efe49721aa69dbee64074f4
jpgerber1986 <jordangerber@gmail.com>
2025-11-10 16:06:31 -0700

    Add dev books

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,14 +14,26 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::create([
+        User::create([
             'email' => 'jordangerber@gmail.com',
             'password' => Hash::make('jordan'),
             'info' => [
-                'name' => 'Jordan Gerber'
+                'name' => 'Jordan Gerber',
+                'books' => [
+                    [
+                        'title' => 'Test Book 1',
+                        'slug' => Str::slug('Test Book 1', '-'),
+                        'hash' => md5(Str::slug('Test Book 1', '-')),
+                        'cover' => null
+                    ],
+                    [
+                        'title' => 'Test Book 2',
+                        'slug' => Str::slug('Test Book 2', '-'),
+                        'hash' => md5(Str::slug('Test Book 2', '-')),
+                        'cover' => null
+                    ],
+                ]
             ],
         ]);
-
-        // User::factory(10)->create();
     }
 }
