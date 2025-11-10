379609541d754466eb6e46270e6b86439bb39cfe
jpgerber1986 <jordangerber@gmail.com>
2025-11-10 14:35:38 -0700

    Renamed private filesystem to books

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,7 +30,7 @@ return [
 
     'disks' => [
 
-        'private' => [
+        'books' => [
             'driver' => 'local',
             'root' => storage_path('app/private'),
             'serve' => true,
